369b07dd53ba2bb9750b776df29fe48da12f7f28
prabhupadswain <prabhupad.swain@outlook.com>
2024-03-21 18:52:26 +0530

    Frontend logic

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='container'>
+      <h1>Sanity Check from React!!</h1>
+      <p>MERN Stack Application</p>
+    </div>
+  );
+}
+
+export default App;
