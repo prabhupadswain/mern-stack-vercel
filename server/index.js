97aea0f161dcf1963278aa2dfdc07e0364238e49
prabhupadswain <prabhupad.swain@outlook.com>
2024-03-21 19:10:50 +0530

    DB connection established

--- server/index.js
+++ server/index.js
@@ -1,12 +1,23 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
+const dotenv = require('dotenv').config();
+
+// Express App
 const app = express();
 
+//Connect to DB
+connectDB();
+
+// Middleware to handle requests
 app.use('/', (req, res) => {
   res.status(200).json({
-    message: 'OK from Server',
+    message: 'OK from Server - Sanity Check',
   });
 });
 
-const PORT = 4000;
+// Set PORT
+const PORT = process.env.PORT || 3500;
+
+// Server running on PORT & listening to requests
 app.listen(PORT, () => console.log(`Server started on PORT:${PORT}`));
