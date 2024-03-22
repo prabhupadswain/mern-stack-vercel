ac2702957359f7f4bc00699784b2f5ea299b9cb1
prabhupadswain <prabhupad.swain@outlook.com>
2024-03-22 21:35:48 +0530

    Bootstrap check at client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className='container'>
       <h1>Sanity Check from React!!</h1>
       <p>MERN Stack Application</p>
+      <p>Bootstrap Check</p>
     </div>
   );
 }
