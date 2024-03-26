c41700f1937818b3a0d4390dc7f8aa46ccd91585
prabhupadswain <prabhupad.swain@outlook.com>
2024-03-26 13:43:54 +0530

    Controlled Inputs in UserForm component

--- client/src/components/UserForm.js
+++ client/src/components/UserForm.js
@@ -1,65 +1,102 @@
-import React, { Fragment } from 'react';
+import React, { useState } from 'react';
 
 const UserForm = () => {
+  const [user, setUser] = useState({
+    username: '',
+    age: 0,
+    salaried: undefined,
+  });
+
+  const changeHandler = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(user);
+
+    const { username, age, salaried } = user;
+
+    const newUser = {
+      username,
+      age: +age,
+      salaried: salaried === 'yes' ? true : false,
+    };
+
+    console.log(newUser);
+
+    setUser({
+      username: '',
+      age: 0,
+      salaried: undefined,
+    });
+  };
+
   return (
-    <Fragment>
-      <form>
-        <div className='row mb-3'>
-          <label htmlFor='userName' className='col-sm-2 col-form-label'>
-            Username:{' '}
-          </label>
-          <div className='col-sm-10'>
-            <input type='email' className='form-control' id='userName' />
+    <form onSubmit={submitHandler}>
+      <div className='row mb-3'>
+        <label className='col-sm-2 col-form-label'>Username: </label>
+        <div className='col-sm-10'>
+          <input
+            type='text'
+            placeholder='Enter username'
+            value={user.username}
+            className='form-control'
+            name='username'
+            onChange={changeHandler}
+          />
+        </div>
+      </div>
+      <div className='row mb-3'>
+        <label className='col-sm-2 col-form-label'>Age: </label>
+        <div className='col-sm-10'>
+          <input
+            type='number'
+            placeholder='Enter user age in number only'
+            value={user.age}
+            className='form-control'
+            name='age'
+            onChange={changeHandler}
+          />
+        </div>
+      </div>
+      <fieldset className='row mb-3'>
+        <legend className='col-form-label col-sm-2 pt-0'>
+          Are you salaried?
+        </legend>
+        <div className='col-sm-10'>
+          <div className='form-check'>
+            <input
+              className='form-check-input'
+              type='radio'
+              name='salaried'
+              value='yes'
+              checked={user.salaried === 'yes'}
+              onChange={changeHandler}
+            />
+            <label className='form-check-label'>Yes</label>
+          </div>
+          <div className='form-check'>
+            <input
+              className='form-check-input'
+              type='radio'
+              name='salaried'
+              value='no'
+              checked={user.salaried === 'no'}
+              onChange={changeHandler}
+            />
+            <label className='form-check-label'>No</label>
           </div>
         </div>
-        <div class='row mb-3'>
-          <label htmlFor='age' className='col-sm-2 col-form-label'>
-            Age:{' '}
-          </label>
-          <div className='col-sm-10'>
-            <input type='number' className='form-control' id='age' />
-          </div>
+      </fieldset>
+      <div className='row mb-3'>
+        <div className='col-sm-4'>
+          <button type='submit' className='btn btn-success'>
+            Add a new user to database
+          </button>
         </div>
-        <fieldset className='row mb-3'>
-          <legend className='col-form-label col-sm-2 pt-0'>
-            Are you salaried?
-          </legend>
-          <div className='col-sm-10'>
-            <div className='form-check'>
-              <input
-                className='form-check-input'
-                type='radio'
-                name='salaried'
-                id='salariedTrue'
-                value='yes'
-              />
-              <label className='form-check-label' htmlFor='salariedTrue'>
-                Yes
-              </label>
-            </div>
-            <div className='form-check'>
-              <input
-                className='form-check-input'
-                type='radio'
-                name='salaried'
-                id='salariedFalse'
-                value='no'
-              />
-              <label className='form-check-label' htmlFor='salariedFalse'>
-                No
-              </label>
-            </div>
-          </div>
-        </fieldset>
-        <div className='row mb-3'>
-          <div className='col-sm-4'>
-            <button type='submit' class='btn btn-success'>
-              Create a new user
-            </button>
-          </div>
-        </div>
-      </form>
-    </Fragment>
+      </div>
+    </form>
   );
 };
 
